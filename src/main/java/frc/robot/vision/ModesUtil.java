7d1ac88a0acc6dc6954a43bfc8c37c92d9264ae7
Jonah Snider <jonah@jonahsnider.com>
2022-01-15 21:03:38 -0800

    Add utility for setting vision mode

---
+++ src/main/java/frc/robot/vision/ModesUtil.java
@@ -0,0 +1,32 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.vision;
+
+/** Used for configuring which mode the vision system is in. */
+public class ModesUtil {
+  public enum Mode {
+    /** Vision is enabled and trying to detect the high target (upper hub). */
+    HIGH_TARGET,
+    /** Vision is not enabled. The LEDs are disabled and a raw camera feed is shown. */
+    RAW_VIDEO
+  }
+
+  private ModesUtil() {}
+
+  public static void setMode(Mode mode) {
+    switch (mode) {
+      case HIGH_TARGET:
+        Limelight.setPipeline(0);
+        Limelight.setCamMode(Limelight.CamMode.VISION_PROCESSOR);
+        break;
+      case RAW_VIDEO:
+        Limelight.setPipeline(9);
+        Limelight.setCamMode(Limelight.CamMode.DRIVER_CAMERA);
+        break;
+      default:
+        throw new IllegalArgumentException("Invalid mode: " + mode);
+    }
+  }
+}
