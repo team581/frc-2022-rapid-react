2ee13acab72d754fb9736506e5a3cd6d5206f7aa
Nathaniel <nathangamz0@gmail.com>
2022-01-19 18:21:02 -0800

    Reverted controller settings from controlled-driving

--- src/main/java/frc/robot/util/ControllerUtil.java
+++ src/main/java/frc/robot/util/ControllerUtil.java
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 // Copyright (c) FIRST and other WPILib contributors.
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
@@ -38,3 +39,22 @@ public class ControllerUtil {
     return joystickScale(thetaLimiter.calculate(controller.getLeftX()));
   }
 }
+=======
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.util;
+
+/** Util functions for joysticks. */
+public final class ControllerUtil {
+  private static final double SCALAR = 3;
+
+  private ControllerUtil() {}
+
+  /** Scale a joystick value. */
+  public static double joystickScale(double x) {
+    return Math.signum(x) * (Math.pow(x, 2) / SCALAR);
+  }
+}
+>>>>>>> Stashed changes
