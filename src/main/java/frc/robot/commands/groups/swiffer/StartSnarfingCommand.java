ef54131799cec534fc4115b0299bcf00bcbf98e4
rkanemoto <73001957+rkanemoto@users.noreply.github.com>
2022-02-26 13:17:10 -0800

    Add lifting to start snarfing command

--- src/main/java/frc/robot/commands/groups/swiffer/StartSnarfingCommand.java
+++ src/main/java/frc/robot/commands/groups/swiffer/StartSnarfingCommand.java
@@ -5,25 +5,30 @@
 package frc.robot.commands.groups.swiffer;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
-import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import frc.robot.commands.groups.lifter.StartLoweringCommand;
+import frc.robot.subsystems.LifterSubsystem;
 import frc.robot.subsystems.SwifferSubsystem;
 
 /** A command that starts the snarfer and waits for it to speed up. */
-public class StartSnarfingCommand extends SequentialCommandGroup {
+public class StartSnarfingCommand extends ParallelCommandGroup {
   /** Creates a new StartSnarfingCommand. */
   public StartSnarfingCommand(
       // gives the subsystem to use
-      SwifferSubsystem swiffer) {
+      SwifferSubsystem swiffer, LifterSubsystem lifter) {
     // makes it where one command can use the subsystem at a time
     addRequirements(swiffer);
     addCommands(
-        new InstantCommand(
-            // creating an instant command that starts snarfing (swiffer is the command that is
-            // snarfing)
-            swiffer::startSnarfing),
-        // TODO Measure the number to actually wait
-        // Created a wait command to wait for the snarfer to spin up
-        new WaitCommand(0.5));
+        // runs everything in parallel
+        new StartLoweringCommand(lifter),
+        sequence(
+            new InstantCommand(
+                // creating an instant command that starts snarfing (swiffer is the command that is
+                // snarfing)
+                swiffer::startSnarfing),
+            // TODO Measure the number to actually wait
+            // Created a wait command to wait for the snarfer to spin up
+            new WaitCommand(0.5)));
   }
 }
