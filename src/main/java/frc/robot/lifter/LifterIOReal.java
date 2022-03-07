cfed0e843050119b971dbc8b5b28303e1ed3995c
Jonah Snider <jonah@jonahsnider.com>
2022-03-06 19:17:24 -0800

    Set lifter encoder measurement period to 1ms

--- src/main/java/frc/robot/lifter/LifterIOReal.java
+++ src/main/java/frc/robot/lifter/LifterIOReal.java
@@ -5,6 +5,7 @@
 package frc.robot.lifter;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
+import com.ctre.phoenix.sensors.SensorVelocityMeasPeriod;
 import frc.robot.Constants;
 
 public class LifterIOReal implements LifterIO {
@@ -19,6 +20,11 @@ public class LifterIOReal implements LifterIO {
         throw new IllegalStateException(
             "The currently configured robot doesn't support this subsystem");
     }
+
+    // TODO: These values probably need to be tuned - see tuning instructions
+    // https://docs.ctre-phoenix.com/en/stable/ch14_MCSensor.html#recommended-procedure
+    motor.configVelocityMeasurementPeriod(SensorVelocityMeasPeriod.Period_1Ms);
+    motor.configVelocityMeasurementWindow(1);
   }
 
   @Override
