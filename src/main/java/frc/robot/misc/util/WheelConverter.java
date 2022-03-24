9682b00bee34e69e2a07da84f6223332b1913a1b
Jonah Snider <jonah@jonahsnider.com>
2022-03-24 15:06:56 -0700

    Create class for converting wheel angles to and from distances

---
+++ src/main/java/frc/robot/misc/util/WheelConverter.java
@@ -0,0 +1,49 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.misc.util;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+
+/** Used for doing math with circles. */
+public class WheelConverter {
+  public static WheelConverter fromRadius(double radius) {
+    return new WheelConverter(radius);
+  }
+
+  public static WheelConverter fromDiameter(double diameter) {
+    return new WheelConverter(diameter / 2);
+  }
+
+  private final double radius;
+
+  private WheelConverter(double radius) {
+    this.radius = radius;
+  }
+
+  /** Converts a rotation in radians to a distance. */
+  public double radiansToDistance(double radians) {
+    return radians * radius;
+  }
+
+  /** Converts a rotation in radians to a distance. */
+  public double radiansToDistance(Rotation2d rotation) {
+    return radiansToDistance(rotation.getRadians());
+  }
+
+  /** Converts a distance to a rotation in radians. */
+  public double distanceToRadians(double distance) {
+    return distance / radius;
+  }
+
+  /** Converts an angular velocity (radians/second) to a velocity (meters/second). */
+  public double angularVelocityToVelocity(double angularVelocity) {
+    return angularVelocity * radius;
+  }
+
+  /** Converts a velocity (distance/second) to an angular velocity (radians/second). */
+  public double velocityToAngularVelocity(double velocity) {
+    return velocity / radius;
+  }
+}
