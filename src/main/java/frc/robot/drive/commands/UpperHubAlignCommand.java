7908d1599377dfa27a9242ac01e3c8365867d10a
Eric Snider <eric@sniderware.com>
2022-04-06 19:45:49 -0700

    Rotate the goal poses by 24º to match the position of the hub

--- src/main/java/frc/robot/drive/commands/UpperHubAlignCommand.java
+++ src/main/java/frc/robot/drive/commands/UpperHubAlignCommand.java
@@ -25,31 +25,32 @@ public class UpperHubAlignCommand extends DynamicTrajectoryFollowCommand {
       UpperHubVisionTarget.RADIUS + Units.feetToMeters(2);
 
   /** Four poses that align you with the fenders while facing toward the center of the hub. */
-  // TODO: The hub is not perfectly aligned, it is slightly rotated. Refer to the field diagram and
-  // update these goal poses to properly align
+  // The hub is not perfectly aligned with the edges of the field.
+  // It is rotated 24º ± 3º (counter clockwise).
+  // Refer to the field diagram:
   // https://firstfrc.blob.core.windows.net/frc2022/FieldAssets/2022LayoutMarkingDiagram.pdf
   private static final List<Pose2d> GOAL_POSES =
-      List.of(
+    List.of(
           // Right
           new Pose2d(
               UpperHubVisionTarget.COORDINATES.plus(
-                  new Translation2d(GOAL_DISTANCE_FROM_HUB_CENTER, 0)),
-              Rotation2d.fromDegrees(180)),
+                  new Translation2d(GOAL_DISTANCE_FROM_HUB_CENTER, 0).rotateBy(Rotation2d.fromDegrees(24))),
+              Rotation2d.fromDegrees(180 + 24 )),
           // Left
           new Pose2d(
               UpperHubVisionTarget.COORDINATES.minus(
-                  new Translation2d(GOAL_DISTANCE_FROM_HUB_CENTER, 0)),
-              Rotation2d.fromDegrees(0)),
+                  new Translation2d(GOAL_DISTANCE_FROM_HUB_CENTER, 0).rotateBy(Rotation2d.fromDegrees(24))),
+              Rotation2d.fromDegrees(0 + 24)),
           // Up
           new Pose2d(
               UpperHubVisionTarget.COORDINATES.plus(
-                  new Translation2d(0, GOAL_DISTANCE_FROM_HUB_CENTER)),
-              Rotation2d.fromDegrees(90)),
+                  new Translation2d(0, GOAL_DISTANCE_FROM_HUB_CENTER).rotateBy(Rotation2d.fromDegrees(24))),
+              Rotation2d.fromDegrees(90 + 24)),
           // Down
           new Pose2d(
               UpperHubVisionTarget.COORDINATES.minus(
-                  new Translation2d(0, GOAL_DISTANCE_FROM_HUB_CENTER)),
-              Rotation2d.fromDegrees(270)));
+                  new Translation2d(0, GOAL_DISTANCE_FROM_HUB_CENTER).rotateBy(Rotation2d.fromDegrees(24))),
+              Rotation2d.fromDegrees(270 + 24)));
 
   private static Pose2d chooseGoalPose(Localization localization) {
     return frc.robot.paths.util.TrajectoryUtil.shortestDistanceTo(
